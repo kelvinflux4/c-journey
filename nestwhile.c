152b6424b44235a2ab957b6bbf6471a80598aba2
kelvinflux4 <kflux@icloud.com>
2022-09-27 15:28:39 +0300

    nested

---
+++ nestwhile.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+    while(i<3)
+    {
+        int i=j;
+        while(j<=3)
+        {
+            printf("%d", j++);
+        }
+        printf("%d", i++);
+    }
+    getch();
+}
