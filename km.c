dea42e0d72fe43d61a8c3443afdb0d2dd6af3257
kelvinflux4 <kflux@icloud.com>
2022-09-27 13:23:20 +0300

    j

---
+++ km.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main (void)
+{
+    int a, i, sum=0;
+   do
+   {
+     printf("Enter number");
+        scanf("%d:", &a);
+           printf("%d:", sum);
+            sum=sum+a;
+    while(a<0);
+            break;
+   }
