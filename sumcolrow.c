2994468a36f9d39272bffe0643b42e6323568ada
kelvinflux4 <kflux@icloud.com>
2022-09-30 00:41:38 +0300

    arrays

---
+++ sumcolrow.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[3][3],i,j, sr, sc;
+    printf("enter element of matrix:\n");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0; i<3; i++)
+    {
+        sr=sc=0;
+        for(j=0; j<3; j++)
+        {
+            sr=sr+a[i][j];
+            sc=sc+a[j][i];
+        }
+        printf("sr=%d , sc=%d ", sr , sc);
+    }
+    printf("\t");
+}
---
+++ sumof2matrix.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[2][3],b[2][3],c[2][3],j,i;
+    printf("enter element of matrix a:\n");
+    for(i=0; i<2; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+      printf("enter element of matrix b");
+    for(i=0; i<2; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    printf("the first matrix:\n");
+    for(i=0; i<2; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("the second matrix:\n");
+    for(i=0; i<2; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("%d\t", b[i][j]);
+        }
+        printf("\n");
+    }
+    printf("the third matrix:\n");
+    for(i=0; i<2; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+}
